1829b3549cb71597a1bce2d86630be6d2ef04212
kleneyle <kleneyle@student.42.fr>
2015-06-18 11:37:06 +0200

    all

---
+++ unityD03/Assets/buttonManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonManager : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void NextLevelButton()
+	{
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+	public void QuitButton()
+	{
+		Application.Quit();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
