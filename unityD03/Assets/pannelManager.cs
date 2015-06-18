7738ed8dac165b05ee6597da3227ac1995197bcf
kleneyle <kleneyle@student.42.fr>
2015-06-18 14:07:48 +0200

    scene

--- unityD03/Assets/pannelManager.cs
+++ unityD03/Assets/pannelManager.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class pannelManager : MonoBehaviour {
 
+	private GUIText myGUIText;
+	public GameObject game;
 	// Use this for initialization
+	
 	void Start () {
-	
+		this.GetComponentInChildren<GUIText> ().text = "toto";
+		
 	}
-	
+	public void Drag(){
+		transform.Translate(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+		game = (GameObject)GameObject.FindGameObjectWithTag ("game");
+		Debug.Log (game.GetComponent<gameManager>().playerEnergy);
+		
+	}
 	// Update is called once per frame
 	void Update () {
-	
+		myGUIText = this.GetComponentInChildren<GUIText> ();
+		//myGUIText. = "toto";
+		myGUIText.color = new Color(1,1,1);
 	}
 }
